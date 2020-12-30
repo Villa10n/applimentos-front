8b63a1c3067f8ff6f7c31c70fca7468fa84056c6
Joaquin <joaquin.villalon@mail.udp.cl>
2020-12-29 21:47:09 -0300

    primera version funcionando

---
+++ src/components/Alimentos/Alimentos.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+import { diaContext } from '../../context/Dia/diaContext';
+import { OrdenarArregloObj } from '../../helpers';
+
+// Componentes
+import { Alimento } from './Alimento';
+
+export const Alimentos = () => {
+
+    // Context
+    const DiaContext = useContext(diaContext);
+    const { guardarAlimentosAPI } = DiaContext;
+
+    // State
+    const [data, guardarData] = useState([]);
+
+    // Consulta a la API
+    useEffect(() => {
+        let request = new Request('https://ancient-beach-97922.herokuapp.com/api/food', {
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'omit',
+            referrerPolicy: 'no-referrer'
+        });
+        // let request = new Request('http://localhost:4000/api/food', {
+        //     method: 'GET',
+        //     mode: 'cors',
+        //     credentials: 'omit',
+        //     referrerPolicy: 'no-referrer'
+        // });
+        fetch(request)
+            .then(response => response.json())
+            .then(dataJSON =>  {
+                guardarData(dataJSON);
+                guardarAlimentosAPI(dataJSON);
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }, []);
+
+    // Extraemos solo los alimentos de la consulta a la API
+    let { alimentos } = data;
+
+    // Montamos o no el componente
+    if (alimentos === undefined) return <h1 className="my-4 text-center bg-blue">CARGANDO ALIMENTOS, POR FAVOR ESPERE...</h1>;
+
+    // Ordenamos el objeto por su atributo nombre
+    let alimentosSort = OrdenarArregloObj(alimentos, 'nombre');
+
+    return (
+        <div>
+            <h1 className="my-4 text-center bg-blue">LISTA DE ALIMENTOS</h1>
+            <div className="row row-cols-1 row-cols-md-3 g-4 screen-alimentos">
+                {
+                    alimentosSort.map(alimento => (
+                        <Alimento
+                            key={alimento._id}
+                            alimento={alimento}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
---
+++ src/components/Dia/Dia.js
@@ -0,0 +1,144 @@
+import React, { useState, useContext } from 'react';
+import swal from 'sweetalert';
+
+import { diaContext } from '../../context/Dia/diaContext';
+
+// Components
+import { Item } from './Item';
+
+export const Dia = () => {
+
+    // Context para extraer las comidas agregas desde Alimentos y obtener caloriasMaximas desde Micuenta
+    const nuevoDiaContext = useContext(diaContext);
+    const { comidasDia, botonCalcular, estadoBotonCalcular } = nuevoDiaContext;
+
+    // Extraer datos de Mi Cuenta desde localStorage
+    let { caloriasTotales } = JSON.parse(localStorage.getItem('datos')) || '';
+
+    // State para guardar los inputs y agregar todo a localStorage para Calendario
+    const [form, guardarForm] = useState({
+        fecha: '',
+        caloriasMaximasAlDia: caloriasTotales,
+        caloriasConsumidasDia: ''
+    });
+    const { fecha, caloriasMaximasAlDia, caloriasConsumidasDia } = form;
+
+    // En caso de no haber aun comidas agregadas a Dia
+    if (comidasDia.length === 0) return (
+        <>
+            <h1 className="my-4 text-center bg-blue">CREAR NUEVO DIA</h1>
+            <p className="my-4 text-center fz-20">Agregue sus comidas desde el menú "Alimentos"</p>
+        </>
+    )
+
+    // Ordenamos alfabeticamente el arreglo de comidas
+    comidasDia.sort();
+
+    // Inputs del formulario
+    const handleInputChange = (e) => {
+        guardarForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    // Formatea la fecha ingresada y calcula las calorias totales consumidas ingresadas
+    const calcularDatosIntroducidos = () => {
+        // Comprobamos que ningun input este en 0
+        for (let i = 0; i < comidasDia.length; i++) {
+            if (comidasDia[i].gramosConsumidos === 0) {
+                guardarForm({
+                    ...form,
+                    caloriasConsumidasDia: 0
+                });
+                swal("Cuidado!", "Uno de los alimentos contiene 0 gramos consumidos", "warning");
+                return 0;
+            }
+        }
+        // Calculamos
+        const arrayCaloriasPorcion = comidasDia.map(comida => comida.gramosConsumidos / comida.porcion * comida.caloriasPorcion);
+        const caloriasConsumidas = arrayCaloriasPorcion.reduce((a, b) => a + b).toFixed(1);
+        document.getElementById('total').innerHTML = caloriasConsumidas;
+        // Guardamos
+        guardarForm({
+            ...form,
+            caloriasConsumidasDia: caloriasConsumidas,
+        });
+        // Activamos el boton GUARDAR
+        estadoBotonCalcular(true);
+        return caloriasConsumidas;
+    };
+
+    // Guardar en calendario lo comido en el dia
+    const guardarDiaCalendario = (e) => {
+        e.preventDefault();
+        if (caloriasMaximasAlDia === '' || caloriasMaximasAlDia === undefined || caloriasMaximasAlDia === 0) {
+            swal("Error!", 'Vaya al menú "Mi Cuenta" he ingrese lo solicitado', "error");
+            return 0;
+        }
+        else if (fecha === '') {
+            swal("Cuidado!", "Agregue una fecha antes de continuar", "warning");
+            return 0;
+        }
+        else if (caloriasConsumidasDia === '') {
+            swal("Cuidado!", "Antes de guardar, presione el boton CALCULAR", "warning");
+            return 0;
+        }
+        else if (!botonCalcular) {
+            swal("Cuidado!", "Antes de guardar, calcule otra vez presionando el boton CALCULAR", "warning");
+            return 0;
+        }
+        // Obtener el arreglo de localStorage
+        let arrayCalendario = JSON.parse(localStorage.getItem('calendario'));
+        arrayCalendario.push(form)
+        localStorage.setItem('calendario', JSON.stringify(arrayCalendario));
+        // Notificacion
+        swal("Perfecto!", 'Los datos han sido guardardos en el menú "Calendario"', "success");
+    };
+
+    return (
+        <>
+            <h1 className="my-4 text-center bg-blue">CREAR NUEVO DIA</h1>
+            <div className="screen-dia">
+                <form className="form-dia" onSubmit={(e) => e.preventDefault()}>
+                    <div className="mb-3 form-date">
+                        <label className="form-label fz-30">FECHA</label>
+                        <input type="date" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="fecha" onChange={handleInputChange} />
+                    </div>
+                    <div className="mb-3">
+                        <p className="form-label text-center fz-30 mb-4">COMIDAS AGREGADAS: (Agreguelas desde el menú "Alimentos")</p>
+                        <div className="row row-cols-1 row-cols-md-3 g-4">
+                            {
+                                comidasDia.map(comida => (
+                                    <Item
+                                        key={comida._id}
+                                        comida={comida}
+                                    />
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <p className="my-4 text-center fz-30">TOTAL DE CALORÍAS CONSUMIDAS</p>
+                    <p className="text-center fz-30" id="total"></p>
+                    <div className="card-body">
+                        <div className="d-grid gap-2 mb-2">
+                            <button
+                                type="button"
+                                className="btn btn-primary fz-20"
+                                onClick={calcularDatosIntroducidos}
+                            >CALCULAR</button>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="d-grid gap-2">
+                        <button
+                            type="button"
+                            className="btn btn-success fz-20"
+                            onClick={guardarDiaCalendario}
+                        >GUARDAR</button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+};
